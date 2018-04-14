f0083e4e0574d83180fb0c66456681a7aa403bca
aryawirasandi <aryawirasandi7153@gmail.com>
2018-04-14 12:41:05 +0700

    First commit

---
+++ app/src/main/java/com/example/aryawirasandi/belaria/Fragment/FragmentProfile.java
@@ -0,0 +1,15 @@
+package com.example.aryawirasandi.belaria.Fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aryawirasandi.belaria.R;
+
+public class FragmentProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+    }
+}
---
+++ app/src/main/java/com/example/aryawirasandi/belaria/Fragment/Fragment_Home.java
@@ -0,0 +1,15 @@
+package com.example.aryawirasandi.belaria.Fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aryawirasandi.belaria.R;
+
+public class Fragment_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment__home);
+    }
+}
