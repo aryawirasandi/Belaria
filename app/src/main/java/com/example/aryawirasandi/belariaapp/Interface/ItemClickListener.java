116344459ee0d634b69a4223edbf848a2370953e
aryawirasandi <aryawirasandi7153@gmail.com>
2018-05-02 10:00:18 +0700

    Memperbaiki database karena tidak ada kolom price pada database

---
+++ app/src/main/java/com/example/aryawirasandi/belariaapp/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.aryawirasandi.belariaapp.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view,int position,boolean isLongClick);
+}
